9f1bf4e4e68740482cb0afc5f03e2f3f1acde243
Oopspw <290256660@qq.com>
2018-12-29 15:34:35 +0800

    set false as productionSourceMap and cssSourceMap

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ module.exports = {
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
-    cssSourceMap: true
+    cssSourceMap: false
   },
 
   build: {
@@ -54,7 +54,7 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
